04721481891cfcb772ef90aa056fa74c0b464474
Varun Thummar <110765105+VARUN949@users.noreply.github.com>
2023-10-24 14:42:37 +0530

    Update App.js

--- apps/src/App.js
+++ apps/src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+// https://ssip2023-backend.onrender.com/ for apis
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          SSIP 2023
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
