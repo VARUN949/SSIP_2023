3b6535d322f09c68261a4831f8321ac1e4174575
Dhruv Bavadiya <dhruvbavadiya99@gmail.com>
2023-10-30 21:32:57 +0530

    try to add cron scheduling

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const cron = require('node-cron'); // Import node-cron package
 const placeRoutes = require("./Routes/routePlace");
 const userRoutes = require("./Routes/userRoute");
 const feedbackRoute = require('./Routes/feedbackRoute')
 const errorMiddleware = require("./Middleware/errors");
+const axios = require('axios');
 
 // Initialize Express app
 const app = express();
 console.log(process.env.PORT)
+
 // Middleware - Body parser for JSON data and Cookie parser
 app.use(express.json());
 app.use(cookieParser());
@@ -43,8 +46,31 @@ app.use("/app/vr1", feedbackRoute);
 // Error handling middleware
 app.use(errorMiddleware);
 
+
+
+// Cron Job - Run every 5 minutes
+cron.schedule('*/5 * * * *', async () => {
+  try {
+    const response = await fetch('http://localhost:12500/fake');
+    if (response.ok) {
+      const data = await response.json();
+      console.log('Fetched data:', data);
+      // Perform further operations with the fetched data, such as saving it to a database or processing it.
+    } else {
+      console.error('Error fetching data:', response.status);
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+});
+
+app.get("/fake",(req,res)=>{
+  res.json({
+    message:"hello fake api"
+  })
+})
+
 // Start the server
-// const port = process.env.PORT;
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
