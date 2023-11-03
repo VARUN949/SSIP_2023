3fef0aea088aeafb3f19b8ff22fe0f539f96f56e
Dhruv Bavadiya <dhruvbavadiya99@gmail.com>
2023-11-03 22:30:41 +0530

    some changes

--- backend/Routes/routePlace.js
+++ backend/Routes/routePlace.js
@@ -4,10 +4,10 @@ const { isAuthenticator, isUserAuthorized } = require("../Middleware/auth");
 const router = express.Router();
 
 router.route("/addplace").post(isAuthenticator, isUserAuthorized("admin"),addPlace);
-router.route("/getplace").get(isAuthenticator,getPlace);
-router.route("/get-bylocation").get(isAuthenticator,loactionByCoordinates);
-router.route("/getgovlocation").get(isAuthenticator,getPlaceByName);
-router.route("/getlocation/category").get(isAuthenticator,getBycategory);
+router.route("/getplace").get(getPlace);
+router.route("/get-bylocation").get(loactionByCoordinates);
+router.route("/getgovlocation").get(getPlaceByName);
+router.route("/getlocation/category").get(getBycategory);
 router.route("/updateplace/:Id").put(isAuthenticator, isUserAuthorized("admin"),updatePlace);
 router.route("/deleteplace/:Id").delete(isAuthenticator, isUserAuthorized("admin"),deletePlace);
 
