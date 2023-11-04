d1cd4a98be190be715e44ca8e33b132fd7a22d5a
varun thummar <Varun>
2023-11-04 12:03:58 +0530

    moongose changes

--- backend/Routes/routePlace.js
+++ backend/Routes/routePlace.js
@@ -3,12 +3,12 @@ const { addPlace, updatePlace, deletePlace, getPlace, getlocation, getgovLocatio
 const { isAuthenticator, isUserAuthorized } = require("../Middleware/auth");
 const router = express.Router();
 
-router.route("/addplace").post(isAuthenticator, isUserAuthorized("admin"),addPlace);
+router.route("/addplace").post(isAuthenticator,addPlace);
 router.route("/getplace").get(getPlace);
 router.route("/get-bylocation").get(loactionByCoordinates);
 router.route("/getgovlocation").get(getPlaceByName);
 router.route("/getlocation/category").get(getBycategory);
-router.route("/updateplace/:Id").put(isAuthenticator, isUserAuthorized("admin"),updatePlace);
-router.route("/deleteplace/:Id").delete(isAuthenticator, isUserAuthorized("admin"),deletePlace);
+router.route("/updateplace/:Id").put(isAuthenticator,updatePlace);
+router.route("/deleteplace/:Id").delete(isAuthenticator,deletePlace);
 
 module.exports = router
